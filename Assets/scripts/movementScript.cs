f373399632b21c9b9cdc5bebdbfa94d362bfa5fc
Hayden Winkler <99040410+Haywinkler05@users.noreply.github.com>
2026-02-11 21:40:17 -0600

    Sample move script added

---
+++ Assets/scripts/movementScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+    GameObject player;
+
+    public float moveSpeed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+
+        Vector3 move = new Vector3(moveX, 0f, moveZ);
+
+
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+    }
+}
