68a139949a3da4433f17082b5610a89463dbc02c
JacobFrayser <98858392+JacobFrayser@users.noreply.github.com>
2026-02-13 23:05:23 -0600

    New Player Movement and Looking Script
    Now uses CharacterController and InputManager for player character. Also created a Prefab for player with attached camera.

---
+++ Assets/scripts/PlayerMotor.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool isGrounded;
+    public float speed = 5f;
+    public float gravity = -12.0f;
+    public float jumpHeight = 1.25f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+    }
+
+    // Receives inputs from InputManager script and apply to character controller
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if(isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+        if(playerVelocity.y < -12.0f)
+        {
+            playerVelocity.y = -12.0f;
+        }
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+    }
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+    }
+}
