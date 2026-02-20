86833e4facc89e5dd34b2d9e79c4d9853f36f8b7
Hayden Winkler <99040410+Haywinkler05@users.noreply.github.com>
2026-02-20 02:32:20 -0600

    Made it so we can change out the walk animations for the wander class

--- Assets/scripts/NPCs/FSM/wanderState.cs
+++ Assets/scripts/NPCs/FSM/wanderState.cs
@@ -13,11 +13,17 @@ public class wanderState : IState
     private Animator animator;   
     private Transform target;
     private float timer;
+    string walkAnim;
+    string turnRightAnim;
+    string turnLeftAnim;
 
-    public wanderState(Transform target, NavMeshAgent agent, Animator animator, float radius = 10f, float timeDuration = 3f)
+    public wanderState(Transform target, NavMeshAgent agent, Animator animator, float radius = 10f, float timeDuration = 3f, string walkAnim = "walk", string turnLeftAnim = "turn left 90", string turnRightAnim = "turn right 90")
     {
         wanderRadius = radius; 
         wanderTimer = timeDuration;
+        this.walkAnim = walkAnim;
+        this.turnLeftAnim = turnLeftAnim;
+        this.turnRightAnim = turnRightAnim;
         this.agent = agent;
         this.target = target;
         this.animator = animator;
@@ -27,7 +33,7 @@ public class wanderState : IState
 
     public void onEnter()
     {
-        animator.Play("Walk");
+        animator.Play(walkAnim);
 
         timer = wanderTimer;
     }
@@ -52,14 +58,14 @@ public class wanderState : IState
 
         if(angle > 15f)
         {
-            animator.Play("turn right 90");
+            animator.Play(turnRightAnim);
         }else if(angle < -15f)
         {
-            animator.Play("turn left 90");
+            animator.Play(turnLeftAnim);
         }
         else
         {
-            animator.Play("walk");
+            animator.Play(walkAnim);
         }
     }
 
