e91a46f7de0a532c6c395bd80fe069c6696a4f46
philla1001 <162478223+philla1001@users.noreply.github.com>
2026-02-22 00:21:04 -0600

    Got the inventory system to work for the capsule player

--- Assets/scripts/Player/PlayerMotor.cs
+++ Assets/scripts/Player/PlayerMotor.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
+
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool isGrounded;
@@ -36,7 +37,12 @@ public class PlayerMotor : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         controller = GetComponent<CharacterController>();
+
+
+
     }
+
+
 
     // Update is called once per frame
     void Update()
